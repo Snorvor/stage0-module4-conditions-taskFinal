5e32f82969cb702441926ccb20f6c02c09e4e240
Snorvor <bulezar@gmail.com>
2025-08-02 10:47:50 -0300

    division and multiply

--- src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class IntegerDivider {
     public void printCompletelyDivided(int dividend, int divider) {
+        int multiplyResult, divisionResult;
+
+        divisionResult = dividend / divider;
+
+        multiplyResult = divisionResult & divider;
+
+        System.out.println(multiplyResult);
     }
 }
